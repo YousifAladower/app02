4666cff9fb95e236b05f16132dc0c0b36396d719
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2023-09-08 23:51:00 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+Color w= Colors.white;
+Color b= Colors.black;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+   
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Flutter App"),
+      ),
+      body:Container(
+       // height: double.infinity,
+        color: b,
+        child: Image.network("https://love-words.net/wp-content/uploads/2019/07/8418-3.jpg"),
+      ) ,
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
